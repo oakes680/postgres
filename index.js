917205d475e4d5cf94e17e659dc24a51c39d3905
oakes680 <chris.oakes@outlook.com>
2020-02-26 17:52:51 -0800

    done

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ server.use('/api',  userRouter)
 
 const port = process.env.DATABASE_URL ||  8888
 
-postgresql - fitted - 98682 
 
 server.listen(port, () => {
   console.log(`you are on ${port}`)
